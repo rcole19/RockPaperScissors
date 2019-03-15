0c859bf26720ee0bd406d39acc845a5f7351849d
rcole19 <46427880+rcole19@users.noreply.github.com>
2019-03-15 05:49:58 -0400

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,79 @@
+let userScore = o;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+
+function getComputerChoice() {
+    const choices = [ 'r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    if (letter === "s") return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}  You win!`;
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} loses ${convertToWord(computerChoice)}  You lost :(`;
+}
+
+function draw(userChoice, computerChoice) {
+    result_p.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}  You draw :(`
+}
+
+function game (userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
+
+
+}
+
